19aa3cb9af864001049d2315db3348efc69f3b10
RamReddy1998 <109741542+RamReddy1998@users.noreply.github.com>
2024-03-18 23:58:43 +0530

    Update main.tf with firewall in master

--- main.tf
+++ main.tf
@@ -1,29 +1,44 @@
 provider "google" {
   project     = "nth-bounty-417116"
-
   region      = "us-east1"
 
 }
 
-resource "google_compute_network" "vpc1" {
+resource "google_compute_network" "vpc2" {
   name                    = "my-first-vpc"
   auto_create_subnetworks = "false"
 
 }
 
-resource "google_compute_subnetwork" "my-custom-subnet1" {
-  name          = "my-custom-subnet-1"
-  ip_cidr_range = "10.255.196.0/24"
-  network       = google_compute_network.vpc1.name
+resource "google_compute_subnetwork" "my-custom-subnet2" {
+  name          = "my-custom-subnet-2"
+  ip_cidr_range = "10.255.197.0/24"
+  network       = google_compute_network.vpc2.name
   region        = "us-east1"
+}
+
+
+resource "google_compute_firewall" "default" {
+  name    = "my-vpc-network"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "my-vpc-network"
 }
 
 
 
 #### test file to push
 
-resource "google_compute_disk" "ram" {
-  name = "ram_reddy_disk"
-  type = "pd-ssd"
-  zone = "us-east1-c"
-}
