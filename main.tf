fe8713c1b43880897a74dfe7759f3aebcf51bc6d
RamReddy1998 <109741542+RamReddy1998@users.noreply.github.com>
2024-03-18 23:56:57 +0530

    Update main.tf with firewall protocol

--- main.tf
+++ main.tf
@@ -18,5 +18,26 @@ resource "google_compute_subnetwork" "my-custom-subnet2" {
 }
 
 
+resource "google_compute_firewall" "default" {
+  name    = "my-vpc-network"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "my-vpc-network"
+}
+
+
 
 #### test file to push
